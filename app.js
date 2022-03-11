027011c9141a975970f79da030ed25538dcda9ee
Idowu Adekale <idowu.s.adekale@gmail.com>
2022-03-11 12:48:36 +0100

    new commit

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 let ExpenseController = (() => {
-    let total = 0, savings = 0, expenses = 0, investments = 0;
+    let total = 0,
+        savings = 0,
+        expenses = 0,
+        investments = 0;
 
     return {
         inputEntry(userInput) {
@@ -35,6 +38,53 @@ let ExpenseController = (() => {
     }
 
 })();
+
+//Beginning of the code to create a cursor in a circle 
+
+let mouseX = 0;
+let mouseY = 0;
+let posX = 0;
+let posY = 0;
+let alpha = 1;
+
+const circle = document.querySelector(".circle");
+
+addEventListener("mousemove", ({ clientX, clientY }) => {
+    mouseX = clientX;
+    mouseY = clientY;
+    // arbitrary high value so that it takes a moment until it actually fades out.
+    alpha = 5;
+});
+
+let prev = 0;
+requestAnimationFrame(function render(now) {
+    requestAnimationFrame(render);
+
+    // expecting a 16ms frame interval, 
+    // check how the current update interval compared to that
+    const factor = (now - prev) / 16;
+    prev = now;
+
+    // how quickly to follow the cursor
+    const speed = .125;
+
+    // adjusted for differences in update interval.
+    posX += factor * speed * (mouseX - posX);
+    posY += factor * speed * (mouseY - posY);
+    // fade out
+    alpha *= Math.pow(.95, factor);
+
+    circle.style.transform = `translate(${posX}px, ${posY}px)`;
+
+    let opacity = Math.min(1, alpha);
+
+    // round the value to closest 1/255 step
+    // opacity ain't more precise and that way we don't set 
+    // "new values" that compute to the same opacity.
+    circle.style.opacity = Math.round(opacity * 255) / 255;
+});
+
+//Ending of the code to create a cursor in a circle
 
 let UIController = (() => {
     let expenseType = 'savings';
@@ -77,9 +127,9 @@ let UIController = (() => {
         setExpenseType(type) {
             console.log('here', type);
             this.expenseType = type;
-            let emoji ="💰";
+            let emoji = "💰";
             if (type === 'savings') {
-                emoji ="💰";
+                emoji = "💰";
                 if (document.querySelector(HTMLStrings.btnSubmitExpense).classList.contains('btn-warning')) {
                     document.querySelector(HTMLStrings.btnSubmitExpense).classList.remove('btn-warning');
                 }
@@ -129,7 +179,7 @@ let UIController = (() => {
             }
         },
 
-        addListItem (inputObj) {
+        addListItem(inputObj) {
             let html, element;
             element = HTMLStrings.expenseList;
 
@@ -150,7 +200,7 @@ let UIController = (() => {
         },
 
         updateOverallTotal(totalValue) {
-            document.querySelector(HTMLStrings.monthBudget).textContent  = "₹ " + this.numberFormat(totalValue);
+            document.querySelector(HTMLStrings.monthBudget).textContent = "₹ " + this.numberFormat(totalValue);
 
             if (totalValue > 0) {
                 if (document.querySelector(HTMLStrings.monthBudget).classList.contains('expense-cost')) {
